332ce048447f85340dd070ed0dc9c40a94000cdb
Mullla <diablo_13_06@mail.ru>
2022-01-15 17:37:39 +0300

    init

---
+++ src/pages/About.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+h1.title В этом приложении:
+
+ul(style="padding-left: 25px")
+  li(style="list-style: disc") Полная настройка шаблона на Vue CLI 
+  li(style="list-style: disc") Установка и настройка Vue-router для Vue.js 3
+  li(style="list-style: disc") Динамическая перерисовка без перезагрузки страницы
+  li(style="list-style: disc") Динамические ссылки
+  li(style="list-style: disc") Обработка несуществующих роутов
+  li(style="list-style: disc") Хеши и моды для роутера 
+  li(style="list-style: disc") Настройка общего конфига
+</template>
